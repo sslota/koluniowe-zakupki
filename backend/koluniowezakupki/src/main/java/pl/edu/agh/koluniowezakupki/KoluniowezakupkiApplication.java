9a81b7a6ab3cc834d23b17b5d4ac2df5d8140f0a
mamikula <mmarcinn17@gmail.com>
2023-03-28 08:41:00 +0200

    Backend initialization

---
+++ backend/koluniowezakupki/src/main/java/pl/edu/agh/koluniowezakupki/KoluniowezakupkiApplication.java
@@ -0,0 +1,13 @@
+package pl.edu.agh.koluniowezakupki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KoluniowezakupkiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KoluniowezakupkiApplication.class, args);
+	}
+
+}
