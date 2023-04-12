ce498ac9615c88ab3296968aa58308e2edcfbf93
mamikula <mmarcinn17@gmail.com>
2023-04-12 20:44:14 +0200

    Settings and catalogs structure fix

--- backend/src/main/java/pl/edu/agh/backend/KoluniowezakupkiApplication.java
+++ backend/src/main/java/pl/edu/agh/backend/KoluniowezakupkiApplication.java
@@ -1,4 +1,4 @@
-package pl.edu.agh.koluniowezakupki;
+package pl.edu.agh.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
