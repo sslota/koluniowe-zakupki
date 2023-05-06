cd0e7d23f9712225654d0d7c5d3b5f822db821be
sslota <szymonslota@student.agh.edu.pl>
2023-05-06 13:42:59 +0200

    sign in and sign up view

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,18 @@
-import './App.css';
-import { Routes, Route } from 'react-router-dom';
-import Lists from './components/Lists';
-import SpecificList from './components/SpecificList';
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Lists from "./components/Lists";
+import SpecificList from "./components/SpecificList";
+import SignIn from "./components/authentications/SignIn";
+import SignUp from "./components/authentications/SignUp";
 
 function App() {
   return (
     <Routes>
-      <Route path='/' element={<Lists />}/>
-      <Route path='/list' element={<Lists />}/>
-      <Route path='/list/:id' element={<SpecificList />}/>
+      <Route path="/" element={<Lists />} />
+      <Route path="/list" element={<Lists />} />
+      <Route path="/list/:id" element={<SpecificList />} />
+      <Route path="/sign-in" element={<SignIn />} />
+      <Route path="/sign-up" element={<SignUp />} />
     </Routes>
   );
 }
