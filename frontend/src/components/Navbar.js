3bebfc9208f7b2305174700b6a9403cf356f6650
mamikula <mmarcinn17@gmail.com>
2023-06-17 13:03:38 +0200

    Refactoring

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -35,7 +35,7 @@ function Navbar() {
     <Disclosure as="nav" className="bg-gray-500">
       {({ open }) => (
         <>
-          <div className="mx-auto max-w-full px-2 sm:px-6 lg:px-8">
+          <div className="position:fixed mx-auto max-w-full px-2 sm:px-6 lg:px-8 z-index:10">
             <div className="relative flex h-16 items-center justify-between">
               <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
                 <Disclosure.Button className="inline-flex items-center justify-center rounded-md p-2 text-gray-800 hover:bg-gray-600 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
--- frontend/src/components/lists/navigations/Map.js
+++ frontend/src/components/lists/navigations/Map.js
@@ -6,34 +6,9 @@ import { Icon } from "leaflet";
 import Routing from "./Routing";
 import "leaflet-routing-machine";
 
-
-// function Map() {
-//   return (
-//     <div className="flex items-center justify-center mx-auto max-w-full py-8 px-2 sm:px-6 lg:px-8">
-//       <img className="w-3/5" src="/route.jpeg" alt="route" />
-
-
-//       <div className="fixed top-16 right-0 py-4 px-2 sm:px-6 lg:px-8">
-//         <DropdownMenu />
-//       </div>
-//     </div>
-//   );
-// }
-
 function Map() {
-  // const position = [50.049683, 19.944544]; // Initial map position
-
-  // const markers = [
-  //   { position: [51.505, -0.09], label: 'Marker 1' },
-  //   { position: [51.51, -0.1], label: 'Marker 2' },
-  //   { position: [51.49, -0.1], label: 'Marker 3' },
-  // ];
-
-  // const path = [[51.505, -0.09], [51.51, -0.1], [51.49, -0.1]]; // Path coordinates
-
-
   return (
-<MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+<MapContainer className="h-screen" center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
   <TileLayer
     attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
